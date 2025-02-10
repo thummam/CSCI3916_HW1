51b5b2b9c9aa85acb53b2ee27397612c468112c6
Mary Thumma <nehareddy4121@gmail.com>
2025-02-09 23:26:00 -0700

    hw1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/', (req, res) => {
 });
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-    console.log('Server is running on port ${port}');
+    console.log(`Server is running on port ${port}`);
 })
 
 module.exports = server;
